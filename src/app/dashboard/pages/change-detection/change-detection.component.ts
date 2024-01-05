a3d3bfe2cf6441c432b6d4ea1cf0444305622a6d
Jorge Chavez <jorge.vicente2108@gmail.com>
2024-01-04 23:25:51 -0600

    change detection usando signals y changeDetactionStrategy.onpush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,14 +1,36 @@
-import { CommonModule } from '@angular/common';
-import {  Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { TitleComponent } from '../../../shared/title/title.component';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [TitleComponent, JsonPipe],
   templateUrl: './change-detection.component.html',
-  styles: `
-    
-  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ChangeDetectionComponent { }
+export default class ChangeDetectionComponent {
+  frameworkAsSignal = signal({
+    name: 'angular',
+    releaseDate: 2016,
+  });
+  frameworkAsProperty = {
+    name: 'angular',
+    releaseDate: 2016,
+  };
+  currentFramework = computed(
+    () => `Change detaction - ${this.frameworkAsSignal().name}`
+  );
+
+  constructor() {
+    setTimeout(() => {
+      this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update((value) => ({ ...value, name: 'React' }));
+      console.log('pasaron 3 segundos');
+    }, 3000);
+  }
+}
