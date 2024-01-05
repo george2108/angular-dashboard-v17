b6654ab816a6bcbabb13917785f52fcf893c7456
Jorge Chavez <jorge.vicente2108@gmail.com>
2024-01-04 22:55:08 -0600

    control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,14 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+
+type Grade = 'A' | 'B' | 'F';
 
 @Component({
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
-  styles: `
-    
-  `,
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ControlFlowComponent { }
+export default class ControlFlowComponent {
+  showContent = signal(true);
+  grade = signal<Grade>('A');
+  frameworks = signal(['Angular', 'React', 'Vue', 'NestJs']);
+  frameworks2 = signal([]);
+
+  toggleContent() {
+    this.showContent.update((value) => !value);
+
+    this.grade.update((value) => {
+      if (value === 'A') {
+        return 'B';
+      } else if (value === 'B') {
+        return 'F';
+      } else {
+        return 'A';
+      }
+    });
+  }
+}
