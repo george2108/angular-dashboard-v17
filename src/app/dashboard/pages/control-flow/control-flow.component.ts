4310d428afc9c74f5944878451916c002bf852a3
Jorge Chavez <jorge.vicente2108@gmail.com>
2024-01-04 23:04:02 -0600

    @Input requerido de titulo

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TitleComponent } from '../../../shared/title/title.component';
 
 type Grade = 'A' | 'B' | 'F';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './control-flow.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
+  selector: 'app-title',
   standalone: true,
   imports: [],
-  templateUrl: './title.component.html',
+  template: `
+    <h1 class="text-3xl mb-5">{{title}}</h1>
+  `,
   styles: ``
 })
 export class TitleComponent {
-
+  @Input({required: true}) title!: string;
 }
